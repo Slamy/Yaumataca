2497092661a8a0f03b44384416f47532d1ff6a87
Andre Zeps <andre.zeps@googlemail.com>
2024-02-02 21:40:29 +0100

    Cleanup of impact joystick
    with packed structs

--- src/handlers/hid_impact.cpp
+++ src/handlers/hid_impact.cpp
@@ -15,6 +15,28 @@
 #include "controller_port.hpp"
 #include "pico/stdlib.h"
 
+struct __attribute__((packed)) Report {
+    uint8_t joy_left_x;
+    uint8_t joy_left_y;
+    uint8_t joy_right_y;
+    uint8_t joy_right_x;
+
+    uint8_t coolie_hat : 4;
+    uint8_t button_left : 1;
+    uint8_t button_top : 1;
+    uint8_t button_bottom : 1;
+    uint8_t button_right : 1;
+
+    uint8_t trigger_l1 : 1;
+    uint8_t trigger_l2 : 1;
+    uint8_t trigger_r1 : 1;
+    uint8_t trigger_r2 : 1;
+    uint8_t button_select : 1;
+    uint8_t button_start : 1;
+    uint8_t stick_click_left : 1;
+    uint8_t stick_click_right : 1;
+};
+
 /**
  * @brief Driver for the noname impact Controller
  * Is a very early cheap USB clone of the Playstation 1 controller.
@@ -22,8 +44,11 @@
 class ImpactHidHandler : public DefaultHidHandler {
     void process_report(std::span<const uint8_t> d) {
 
+        auto dat = reinterpret_cast<const Report *>(d.data());
+
 #ifdef DEBUG_PRINT
-        PRINTF("Impact:");
+        PRINTF("Impact: %d %d%d%d%d", dat->coolie_hat, dat->button_left,
+               dat->button_right, dat->button_top, dat->button_bottom);
         for (uint8_t i : d) {
             PRINTF(" %02x", i);
         }
@@ -32,14 +57,13 @@ class ImpactHidHandler : public DefaultHidHandler {
 
         GamepadReport aj;
 
-        uint8_t hat_dir = (d[4] & 0x0F); // Coolie Hat D-Pad
-        aj.update_from_coolie_hat(hat_dir);
+        aj.update_from_coolie_hat(dat->coolie_hat); // Coolie Hat D-Pad
 
-        aj.fire = d[4] & 0x10;
-        aj.sec_fire = d[4] & 0x20;
-        aj.auto_fire = d[4] & 0x40;
+        aj.fire = dat->button_left || dat->button_right;
+        aj.sec_fire = dat->button_bottom;
+        aj.auto_fire = dat->button_top;
 
-        aj.joystick_swap = d[5] & 0x10; // Select button
+        aj.joystick_swap = dat->button_select;
 
         if (target_) {
             target_->process_gamepad_report(aj);
